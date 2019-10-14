9f6a4f26b88bdc68f3610cc29ca35c206d62477d
Rathma <wrathofrathma@gmail.com>
2019-10-14 19:03:08 -0400

    Finished task4

--- COSC350/lab4/task3.2.c
+++ COSC350/lab4/task3.2.c
@@ -1,2 +1,18 @@
 /* Test for task 5.1
  * Execute hello by using symbolic link toHello. Try to delete a file, make directory by using symbolic link toDir21.*/
+
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <unistd.h>
+ #include <sys/stat.h>
+ #include <fcntl.h>
+ #include <sys/wait.h>
+ #include <sys/types.h>
+
+ int main(){
+   remove("toHello");
+   remove("toDir12");
+   remove("hello");
+   mkdir("toDir12/world",0777);
+   return 0;
+ }
