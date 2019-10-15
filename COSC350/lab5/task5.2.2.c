ecab91dfd45f5dd844bc9908f3e14f01574dc5b8
wrathofrathma <wrathofrathma@gmail.com>
2019-10-15 11:32:30 -0400

    This wasn't even supposed to be here.

--- COSC350/lab5/task5.2.2.c
+++
@@ -1,6 +0,0 @@
-/*
-A. Add code in your main function to loop through the utmp file, reading each record. For each record, just print the user name.
-
-B. Add code in the loop to count the number of records that are USER_PROCESS records(logged-in user). When the loop is done, print the number of logged-in users found. Check your results for correctness.
-
-*/
