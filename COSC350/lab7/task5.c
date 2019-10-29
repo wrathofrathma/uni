a272d87ba805da3b2977ca26564297c702d644d6
wrathofrathma <wrathofrathma@gmail.com>
2019-10-29 06:48:55 -0400

    Task 5 complete

--- COSC350/lab7/task5.c
+++ COSC350/lab7/task5.c
@@ -4,3 +4,50 @@
  * The signal SIGINT is usually bound to ^C by the terminal driver. The SIGINT signal terminates the program by default.
  * The signal SIGQUIT is usually bound to ^\ by the terminal driver. The signal SIGQUIT by default causes the program to terminate and creates a core file.
  */
+
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int handler(int sig){
+  if(sig==SIGQUIT)
+    printf("SIGQUIT pressed during the loop\n");
+  else if(sig==SIGINT)
+    printf("SIGINT pressed during the loop\n");
+}
+
+int main(){
+  
+  sigset_t new_set, old_set;
+  sigemptyset(&new_set); //Clear all signals 
+  // Add the two signals to the signal set
+  sigaddset(&new_set, SIGINT);
+  sigaddset(&new_set, SIGQUIT);
+  //Overwrite the signal set.
+  sigprocmask(SIG_BLOCK, &new_set, &old_set);
+
+  int i=0;
+  /* First loop where SIGINT and SIGQUIT are blocked */
+  for(i=0; i<5; i++){
+    printf("First loop(SIGINT & SIGQUIT blocked): %d\n", i);
+    sleep(1);
+  }
+
+  printf("Unblocking SIGQUIT\n");
+  //Now, let's redirect the handler to a custom handler for the unblock operation, since if we don't it'll exit.
+  signal(SIGINT, handler);
+  signal(SIGQUIT, handler);
+  //Reset to default mask
+  sigprocmask(SIG_SETMASK, &old_set, NULL);
+  //Edit our mask to only include SIGINT
+  sigdelset(&new_set, SIGQUIT);
+  sigprocmask(SIG_BLOCK, &new_set, NULL);
+  //Reset the handlers to default
+  signal(SIGINT, SIG_DFL);
+  signal(SIGQUIT, SIG_DFL);
+  /* Second loop where only SIGINT is blocked */
+  for(i=0; i<5; i++){
+    printf("Second loop(SIGINT blocked): %d\n", i);
+    sleep(1);
+  }
+}
